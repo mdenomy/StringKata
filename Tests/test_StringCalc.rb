b70d212190874adf569ad8270fd4abeff4543b5f
Michael Denomy <mdenomy@gmail.com>
2012-01-24 15:48:21 -0500

    Check for ending newline.  Added method to check for format

--- Tests/test_StringCalc.rb
+++ Tests/test_StringCalc.rb
@@ -22,4 +22,27 @@ class TestStringCalc < Test::Unit::TestCase
     calc = StringCalc.new
     assert_equal 15, calc.add("1,2,3,4,5")
   end
+  
+  def test_newlines_as_delimiters
+    calc = StringCalc.new
+    assert_equal 3, calc.add("1\n2")
+  end
+
+  def test_mixed_delimiters
+    calc = StringCalc.new
+    assert_equal 6, calc.add("1\n2,3")
+  end
+
+  def test_extra_whitespace
+    calc = StringCalc.new
+    assert_equal 6, calc.add("1\n 2,\t 3")
+  end
+
+  def test_extra_delimiter
+    calc = StringCalc.new
+    assert_raise ArgumentError do
+	    calc.add("1,2\n")
+	end
+  end
+  
 end
--- app/string_calc.rb
+++ app/string_calc.rb
@@ -1,9 +1,16 @@
 class StringCalc
 
   def add(input)
-    numbers = input.scan(/\d/)
 	value = 0
+	check_format(input)
+    numbers = input.strip.scan(/\d/)
 	numbers.each { |n| value += n.to_i }
 	return value
+  end
+  
+  def check_format(input)
+	if input.end_with? "\n"
+	    raise ArgumentError, "Ends with newline"
+	end
   end
 end
